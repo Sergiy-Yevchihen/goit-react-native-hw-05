9a74e9c10f21ae29760d52121959865422b62d42
Serhii Yevchishen <magodza06@gmail.com>
2023-07-10 17:22:09 +0300

    add nev app

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableWithoutFeedback, Keyboard } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import Navigation from "./src/Screens/Navigation/Navigation";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <NavigationContainer>
+        <Navigation />
+      </NavigationContainer>
+    </TouchableWithoutFeedback>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
